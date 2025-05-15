5ffe67d775a4846bf43ad4dde29b0933c6abff72
Aage Barlund <barlund@gmail.com>
2025-05-15 10:10:13 +0200

    ikon

--- src/data/tjenester.js
+++ src/data/tjenester.js
@@ -7,7 +7,7 @@ export default [
       "Skal du kjøpe bolig? Det er mest sannsynlig den største investeringen du noen gang gjør. Mange føler seg usikker og du som kjøper vil jo unngå fallgruver. Kommer store utgifter på boligen i fremtiden? Og hvilke utgifter?",
     avsnitt2:
       "I følge avhendingsloven, som regulerer kjøp og salg av fast eiendom, kan ikke kjøper gjøre gjeldende mangler han kjente eller måtte kjenne til ved avtaleinngåelse. I ettertid kan det være vanskelig å få erstatning om man oppdager feil eller mangler med boligen som er opplyst i salgsprospektet. Våre medarbeidere har lang erfaring med å vurdere boligens tilstand, tekniske verdi og markedsverdi. Vi kan hjelpe deg med gjennomlesning av dokumenter, og gir deg konkrete tilbakemeldinger på hva som bør undersøkes nærmere, samt gode råd og tips.",
-    ikon: "",
+    ikon: "../icons/home-modern",
     bilde: "husBilde",
   },
   {
@@ -17,7 +17,7 @@ export default [
       "En tilstandsrapport er en grundig og detaljert beskrivelse av boligens status med hensyn til teknisk tilstand og vedlikehold. I en tilstandsrapport fokuseres det spesielt på områder som er viktig i forbindelse med salg av eiendom. Tilstandsrapporten beskriver avvik, der vi tar med bestemte referansenivåer som byggeforskrifter, eiendommens tilstand etter slitasje og aldring – kort og godt boligens tilstand på nåværende tidspunkt.",
     avsnitt2:
       "Tilstandsrapport er det som er foretrukket i forbindelse med kjøp og salg av eiendom, både av kjøper, selger og meglere.",
-    ikon: "",
+    ikon: "../icons/home2",
     bilde: "",
     bakgrunn: "",
   },
@@ -27,7 +27,7 @@ export default [
     avsnitt1:
       "En verditakst av din bolig gir deg en vurdering av markedsverdien på boligen/eiendommen på befaringstidspunktet. Det legges da vekt på boligens størrelse, standard og beliggenhet. Denne benyttes i forbindelse med boliglån eller boligkreditt på eiendommen din der bankene ber deg om å dokumentere verdien på boligen.",
     avsnitt2: "",
-    ikon: "",
+    ikon: "../icons/banknotes",
     bilde: "",
     bakgrunn: ""
   },
@@ -38,7 +38,7 @@ export default [
       "Forhåndstakst er en verditakst som gjelder en bygning eller en ikke oppført del av bygning.",
     avsnitt2:
       "Forhåndstakst av eiendom gjøres etter de samme prinsipper som ved en verditakst, men vurderingene av bygget gjøres bare på grunnlag av tegninger og teknisk beskrivelse.",
-    ikon: "",
+    ikon: "../icons/banknotes",
     bilde: "",
     bakgrunn: ""
   },
@@ -48,7 +48,7 @@ export default [
     avsnitt1:
       "Brukes i forbindelse med refinansiering. Ved for eksempel søknad om bedre lånevilkår, da er en oppdatert takst eller verdivurdering ofte nødvendig. Dette kan være et krav fra finansinstitusjonen. Da kreves det ofte at dokumentasjonen ikke er eldre enn seks måneder.  Verdivurdering er utviklet sammen med finansinstitusjonene.",
     avsnitt2: "",
-    ikon: "",
+    ikon: "../icons/banknotes",
     bilde: "",
     bakgrunn: "",
   },
@@ -70,7 +70,7 @@ export default [
       "En verditaksering av næringseiendom baseres blant annet på kontantstrømanalyse, sammenlignende salgsverdier, avkastningskrav, tekniske verdier, leiekontrakter og markedsleie.",
     avsnitt2:
       "Ved verditaksering av næringseiendom er det vanligvis leieinntektene som legges til grunn. Det utarbeides en omfattende rapport der de enkelte leiekontrakter nevnes. Områdets generelle leienivå for næringsbygg blir vurdert, samt hensyn til byggets kvalitet og beliggenhet.",
-    ikon: "",
+    ikon: "../icons/industry",
     bilde: "",
     bakgrunn: ""
   },
@@ -81,7 +81,7 @@ export default [
       "Ved skade møter vi kunder i situasjoner som ofte oppleves dramatisk og hvor store verdier står på spill. Når en eiendom får skade kan vi sikre et profesjonelt grunnlag for erstatningen. Takstmannens rolle som uavhengig tredjepart er i denne sammenhengen en viktig faktor for å sikre at skadelidte får riktig oppgjør og reparasjon.",
     avsnitt2:
       "I mange tilfeller er det forsikringsselskapet som tar kontakt med oss for å innhente et faglig grunnlag for senere erstatningsutmåling. I noen tilfeller, spesielt ved kompliserte skader, får takstmannen også i oppdrag å styre arbeidene med istandsetting. Langli Takst utfører også naturskadetaksering i hele landet og er godkjent i Norsk Naturskadepool.",
-    ikon: "",
+    ikon: "../icons/flood",
     bilde: "",
     bakgrunn: ""
   },
@@ -92,7 +92,7 @@ export default [
       "Reklamasjonsrapporten er i prinsippet en tilstandsrapport hvor oppdraget er å foreta en vurdering av reklamerte forhold ved eiendommen i forbindelse med eierskifte. Oppdraget går ut på å vurdere de reklamerte forhold på stedet, samt tilgjengelig teknisk saksdokumentasjon.",
     avsnitt2:
       "Reklamasjonsrapporten omfatter ikke vurdering av de juridiske sidene i saken og vurderer kun de forhold som det er reklamert på. De økonomiske konsekvenser av reklamasjonen beregnes med utbedring til referansenivået dersom det konstateres et negativt avvik. Det gjøres fradrag for eventuelle forbedringer som følge av at levetid på bygningsdeler forlenges eller at tilstanden på bygningsdeler blir fornyet.",
-    ikon: "",
+    ikon: "../icons/gavel",
     bilde: "",
     bakgrunn: "",
   },
@@ -102,7 +102,7 @@ export default [
     avsnitt1:
       "I byggelånsoppfølging har takstmannen i oppdrag å besiktige byggeplassen for å vurdere fremdriften og at arbeidet er i rute, samt at kontraktsmessige byggearbeider følges. Da foretas det uanmeldte befaringer under byggeprosessen for å kontrollere utførelser som senere blir innebygd og skjult. Vi vet av erfaring med uavhengig kontroll at dette kan sikre byggherre mot uheldige utførelser som senere kan bli kostbare å rette opp.",
     avsnitt2: "",
-    ikon: "",
+    ikon: "../icons/magnify",
     bilde: "",
   },
   {
@@ -112,7 +112,7 @@ export default [
       "Ved en uavhengig kontroll skal takstmannen påse at det er gjennomført kvalitetssikring av byggearbeidene, at prosjekteringen av tiltaket oppfyller kravene i byggeteknisk forskrift (TEK17) og at byggearbeidene er utført i henhold til tegninger og arbeidsbeskrivelser. Dette for å kontrollere at det benyttes riktig kvalitet og redusere byggefeil.",
     avsnitt2:
       "Tiltaksklasse 1: Her kreves det uavhengig kontroll av søknadspliktige våtrom samt lufttetthet i nybygg.",
-    ikon: "",
+    ikon: "../icons/tape-measure",
     bilde: "",
   },
   {
@@ -121,7 +121,7 @@ export default [
     avsnitt1:
       "De fleste kan spare mye på å iverksette ulike tiltak for å spare strøm eller produsere energi på en smartere måte. Dette kan inkludere alt fra etterisolering og tetting av vinduskarmer til installasjon av solcellepanel og varmepumpesystem.",
     avsnitt2: "",
-    ikon: "",
+    ikon: "../icons/home-energy",
     bilde: "",
   },
   {
@@ -130,7 +130,7 @@ export default [
     avsnitt1: "Enovatilskuddet gir støtte til gode energi- og klimatiltak.",
     avsnitt2:
       "Vi har energirådgiver som utfører rapporter gjennom Enova sitt rapporteringssystem.",
-    ikon: "",
+    ikon: "../icons/solar-energy",
     bilde: "",
   },
 ];
