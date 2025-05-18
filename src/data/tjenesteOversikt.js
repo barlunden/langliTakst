c6c03ddfd91788f73dca677e7fec54eda092f010
Aage Barlund <barlund@gmail.com>
2025-05-18 18:48:02 +0200

    css

--- src/data/tjenesteOversikt.js
+++ src/data/tjenesteOversikt.js
@@ -5,8 +5,8 @@ export default [
     slug: "kjopogsalg",
     tittel: "Kjøp og salg av bolig",
     avsnitt1:
-      "Trygg bolighandel starter med riktig rådgivning. Vi hjelper deg med vurdering av bolig før kjøp eller salg, gjennomgang av dokumenter, og gir deg råd om hva du bør være oppmerksom på for å unngå ubehagelige overraskelser.",
-    avsnitt2: "",
+      "Trygg bolighandel starter med riktig rådgivning.",
+    avsnitt2: "Vi hjelper deg med vurdering av bolig før kjøp eller salg, gjennomgang av dokumenter, og gir deg råd om hva du bør være oppmerksom på for å unngå ubehagelige overraskelser.",
     ikon: "../icons/home-modern",
     bakgrunn: "hsl(48, 100%, 96%)",
   },
@@ -15,8 +15,8 @@ export default [
     slug: "takst",
     tittel: "Taksering og verdivurdering",
     avsnitt1:
-      "Få en profesjonell og grundig vurdering av bolig, tomt eller næringseiendom. Vi tilbyr ulike former for taksering og verdivurdering som gir deg trygghet og dokumentasjon ved kjøp, salg, finansiering eller forsikringsoppgjør.",
-    avsnitt2: "",
+      "Få en profesjonell og grundig vurdering av bolig, tomt eller næringseiendom.",
+    avsnitt2: "Vi tilbyr ulike former for taksering og verdivurdering som gir deg trygghet og dokumentasjon ved kjøp, salg, finansiering eller forsikringsoppgjør.",
     ikon: "../icons/magnify",
     bakgrunn: "hsl(220, 60%, 96%)",
   },
@@ -25,8 +25,8 @@ export default [
     slug: "byggkontroll",
     tittel: "Byggeprosjekt og kontroll",
     avsnitt1:
-      "Sikre kvalitet og fremdrift i byggeprosjektet ditt. Vi tilbyr uavhengig kontroll og byggelånsoppfølging, slik at du som byggherre får en trygg og profesjonell oppfølging gjennom hele byggeprosessen.",
-    avsnitt2: "",
+      "Sikre kvalitet og fremdrift i byggeprosjektet ditt.",
+    avsnitt2: "Vi tilbyr uavhengig kontroll og byggelånsoppfølging, slik at du som byggherre får en trygg og profesjonell oppfølging gjennom hele byggeprosessen.",
     ikon: "../icons/tape-measure",
     bakgrunn: "hsl(100, 40%, 96%)",
   },
@@ -35,8 +35,8 @@ export default [
     slug: "energi",
     tittel: "Energi og miljø",
     avsnitt1:
-      "Reduser energikostnadene og gjør boligen mer miljøvennlig. Vi tilbyr energirådgivning og hjelper deg med å finne gode løsninger og støtteordninger, slik som Enovatilskuddet, for en mer bærekraftig bolig.",
-    avsnitt2: "",
+      "Reduser energikostnadene og gjør boligen mer miljøvennlig.",
+    avsnitt2: " Vi tilbyr uavhengig kontroll og byggelånsoppfølging, slik at du som byggherre får en trygg og profesjonell oppfølging gjennom hele byggeprosessen.",
     ikon: "../icons/solar-energy",
     bakgrunn: "hsl(140, 40%, 96%)",
   },
