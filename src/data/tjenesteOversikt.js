82ae3e84705835e25739656ba460cd6f3525464e
Aage Barlund <barlund@gmail.com>
2025-05-18 08:56:07 +0200

    Css og tjenestekort

--- src/data/tjenesteOversikt.js
+++ src/data/tjenesteOversikt.js
@@ -2,12 +2,21 @@
 export default [
   {
     kategori: "ingress",
+    tittel: "Kjøp og salg av bolig",
+    avsnitt1:
+      "Trygg bolighandel starter med riktig rådgivning. Vi hjelper deg med vurdering av bolig før kjøp eller salg, gjennomgang av dokumenter, og gir deg råd om hva du bør være oppmerksom på for å unngå ubehagelige overraskelser.",
+    avsnitt2: "",
+    ikon: "../icons/home-modern",
+    bakgrunn: "hsl(48, 100%, 96%)",
+  },
+  {
+    kategori: "ingress",
     tittel: "Taksering og verdivurdering",
     avsnitt1:
       "Få en profesjonell og grundig vurdering av bolig, tomt eller næringseiendom. Vi tilbyr ulike former for taksering og verdivurdering som gir deg trygghet og dokumentasjon ved kjøp, salg, finansiering eller forsikringsoppgjør.",
     avsnitt2: "",
-    ikon: { navn: "HomeModern"},
-    bilde: "",
+    ikon: "../icons/magnify",
+    bakgrunn: "hsl(220, 60%, 96%)",
   },
   {
     kategori: "ingress",
@@ -15,17 +24,8 @@ export default [
     avsnitt1:
       "Sikre kvalitet og fremdrift i byggeprosjektet ditt. Vi tilbyr uavhengig kontroll og byggelånsoppfølging, slik at du som byggherre får en trygg og profesjonell oppfølging gjennom hele byggeprosessen.",
     avsnitt2: "",
-    ikon: "",
-    bilde: "",
-  },
-  {
-    kategori: "ingress",
-    tittel: "Kjøp og salg av bolig",
-    avsnitt1:
-      "Trygg bolighandel starter med riktig rådgivning. Vi hjelper deg med vurdering av bolig før kjøp eller salg, gjennomgang av dokumenter, og gir deg råd om hva du bør være oppmerksom på for å unngå ubehagelige overraskelser.",
-    avsnitt2: "",
-    ikon: "",
-    bilde: "",
+    ikon: "../icons/tape-measure",
+    bakgrunn: "hsl(100, 40%, 96%)",
   },
   {
     kategori: "ingress",
@@ -33,7 +33,7 @@ export default [
     avsnitt1:
       "Reduser energikostnadene og gjør boligen mer miljøvennlig. Vi tilbyr energirådgivning og hjelper deg med å finne gode løsninger og støtteordninger, slik som Enovatilskuddet, for en mer bærekraftig bolig.",
     avsnitt2: "",
-    ikon: "",
-    bilde: "",
+    ikon: "../icons/solar-energy",
+    bakgrunn: "hsl(140, 40%, 96%)",
   },
 ];
