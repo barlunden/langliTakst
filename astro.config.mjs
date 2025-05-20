24a37fc1085241271edcc2744472cd371a12070f
Aage Barlund <barlund@gmail.com>
2025-05-20 13:31:28 +0200

    skjema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import 'dotenv/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: 'server'
 });
