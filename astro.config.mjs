d1af842ac7b5ea657bcf537d8969ac62f04b9c44
Aage Barlund <barlund@gmail.com>
2025-05-20 13:46:25 +0200

    netlify update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 import 'dotenv/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server'
+  output: 'server',
+  adapter: netlify(),
 });
